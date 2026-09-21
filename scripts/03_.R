d8a6cd421cf526bc24252f39ca2715f9c89ee647
Antony <antony20jose@gmail.com>
2026-09-21 15:42:23 +0530

    Update and rename 02_DataWrangling.R to 03_.R

---
+++ scripts/03_.R
@@ -0,0 +1,7 @@
+# Load packages
+library(tidyverse) #has dplyr, ggplot2, tidyr and others.
+library(DT) #interactive, searchable HTML tables.
+library(plotly) #converts ggplot charts into interactive ones.
+library(gt) #formatted, publication-style tables.
+
+
